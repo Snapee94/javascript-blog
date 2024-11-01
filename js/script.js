7e71aef0813bbc5fb31a23458bd42b77b34097c3
Kacper <kacpergabis225@gmail.com>
2024-11-01 22:29:18 +0100

    artykuly-klik

--- js/script.js
+++ js/script.js
@@ -1,29 +1,31 @@
 'use strict';
 
 function titleClickHandler(event) {
+    const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event);
-    /* remove class 'active' from all article links  */
 
-    /* add class 'active' to the clicked link */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
 
-    /* remove class 'active' from all articles */
+    clickedElement.classList.add('active');
 
-    /* get 'href' attribute from the clicked link */
+    const activeArticles = document.querySelectorAll('.posts .post.active');
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }
 
-    /* find the correct article using the selector (value of 'href' attribute) */
+    const articleSelector = clickedElement.getAttribute('href');
+    const targetArticle = document.querySelector(articleSelector);
 
-    /* add class 'active' to the correct article */
+    if (targetArticle) {
+        targetArticle.classList.add('active');
+    }
 }
 
 const links = document.querySelectorAll('.titles a');
 
 for (let link of links) {
     link.addEventListener('click', titleClickHandler);
-}
-
-const activeArticles = document.querySelectorAll('.posts .post.active');
-
-for (let activeArticle of activeArticles) {
-    activeArticle.classList.remove('active');
 }
